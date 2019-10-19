176aa4ddb2a466983c583aa72ec135eaf493c8b4
Nina <nblyznuk@gmail.com>
2019-10-19 10:42:38 +0300

    newlesson

--- Project1/src/com/company/lesson3/Lesson31.java
+++ Project1/src/com/company/lesson3/Lesson31.java
@@ -9,8 +9,8 @@ public class Lesson31 {
         System.out.println("ttt");
         System.out.println("ttt");
         System.out.println("ttt");
-        System.out.println("ttt");
-        System.out.println("ttt");
+        System.out.println("677");
+        System.out.println("678");
     }
 
 }
